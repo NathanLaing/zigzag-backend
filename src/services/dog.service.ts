e311a251ef637e6565eecd648aa204881838a319
Nathan Laing <ktmlaing@gmail.com>
2022-08-05 20:47:17 +0200

    feat: add more dogs

--- src/services/dog.service.ts
+++ src/services/dog.service.ts
@@ -31,6 +31,36 @@ export class DogService {
       description:
         'The Golden Retriever is a Scottish breed of retriever dog of medium size. It is characterised by a gentle and affectionate nature and a striking golden coat. It is commonly kept as a pet and is among the most frequently registered breeds in several Western countries. It is a frequent competitor in dog shows and obedience trials; it is also used as a gundog, and may be trained for use as a guide dog. The breed was created by Sir Dudley Marjoribanks at his Scottish estate Guisachan in the late nineteenth century. He cross-bred Flat-coated Retrievers with Tweed Water Spaniels, with some further infusions of Red Setter, Labrador Retriever and Bloodhound. The breed was recognised by the Kennel Club in 1913, and during the interwar period spread to many parts of the world.',
     },
+    {
+      id: 5,
+      name: 'Labrador',
+      imageUrl:
+        'https://2.bp.blogspot.com/-bFIZLg6Razs/V73j1vPc3iI/AAAAAAAACwk/LipzjMZ9WLQMT0VpOrb8T0ET912OUaccACK4B/s1600/choco%2Blab%2B1.jpg',
+      description: `Labrador is a geographic and cultural region within the Canadian province of Newfoundland and Labrador. It is the primarily continental portion of the province and constitutes 71% of the province's area but is home to only 6% of the province's population. It is separated from the island of Newfoundland by the Strait of Belle Isle. It is the largest and northernmost geographical region in Atlantic Canada. Labrador occupies most of the eastern part of the Labrador Peninsula. It is bordered to the west and the south by the Canadian province of Quebec. Labrador also shares a small land border with the Canadian territory of Nunavut on Killiniq Island. The indigenous peoples of Labrador include the Northern Inuit of Nunatsiavut, the Southern Inuit-Métis of NunatuKavut, and the Innu of Nitassinan. `,
+    },
+    {
+      id: 6,
+      name: 'Greyhound',
+      imageUrl:
+        'https://vetstreet.brightspotcdn.com/dims4/default/615618f/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fbd%2Ff32000a38511e087a80050568d634f%2Ffile%2FItalian-Greyhound-5-062411.jpg',
+      description: `The English Greyhound, or simply the Greyhound, is a breed of dog, a sighthound which has been bred for coursing game and greyhound racing. Since the rise in large-scale adoption of retired racing Greyhounds, the breed has seen a resurgence in popularity as a family pet. Greyhounds are defined as a tall, muscular, smooth-coated, "S-shaped" type of sighthound with a long tail and tough feet. Greyhounds are a separate breed from other related sighthounds, such as the Italian greyhound. The Greyhound is a gentle and intelligent breed whose combination of long, powerful legs, deep chest, flexible spine, and slim build allows it to reach average race speeds exceeding 64 kilometres per hour (40 mph). The Greyhound can reach a full speed of 70 kilometres per hour (43 mph) within 30 metres (98 ft), or six strides from the boxes, traveling at almost 20 metres per second (66 ft/s) for the first 250 metres (820 ft) of a race.`,
+    },
+    {
+      id: 7,
+      name: 'Chihuahua',
+      imageUrl:
+        'https://www.fidosavvy.com/images/chihuahua_puppy_closeup_650_c.jpg',
+      description:
+        'The Chihuahua is one of the smallest breeds of dog, and is named after the Mexican state of Chihuahua.',
+    },
+    {
+      id: 8,
+      name: 'German Shepherd',
+      imageUrl:
+        'https://t4.ftcdn.net/jpg/04/75/62/59/360_F_475625971_HTK8MdH194iKiPdBGVHUxPJWNgDHJZEQ.jpg',
+      description:
+        'The German Shepherd or Alsatian is a German breed of working dog of medium to large size. The breed was developed by Max von Stephanitz using various traditional German herding dogs from 1899. It was originally bred as a herding dog, for herding sheep. It has since been used in many other types of work, including disability assistance, search-and-rescue, police work and warfare. It is commonly kept as a companion dog, and according to the Fédération Cynologique Internationale had the second-highest number of annual registrations in 2013.',
+    },
   ];
 
   getDog(id: number): Dog {
